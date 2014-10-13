c5945da88f4e84dad6a23f154a9de3b12e4e57c8
Pontus Mårdnäs <pontus@mardnas.se>
2014-10-13 13:59:52 +0200

    Mostly got test projects compiling & running in cmake.

--- source/orzTest/main.cpp
+++ source/orzTest/main.cpp
@@ -2,10 +2,12 @@
 
 std::wstring g_datapath;
 
-int wmain(int argc, wchar_t* argv[])
-{
-	if(argc<2) return 1;
-	g_datapath=argv[1];
+int wmain(int argc, wchar_t* argv[]) {
+    if (argc < 2) {
+        std::cout << "Data path argument not set, test run failed\n";
+        return 1;
+    }
+    g_datapath = argv[1];
 
-	return UnitTest::RunAllTests();
+    return UnitTest::RunAllTests();
 }
