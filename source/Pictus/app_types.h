2fd9196c71aa909171d65d709165398b7affcddc
Pontus Mårdnäs <pontus@mardnas.se>
2015-09-13 17:44:18 +0200

    Removed explicit values from KeyAction enum

--- source/Pictus/app_types.h
+++ source/Pictus/app_types.h
@@ -13,31 +13,31 @@ namespace App {
 	};
 
 	enum class KeyAction {
-		Undefined = 0,
-		NextImage = 1,
-		NextSkipImage = 2,
-		PreviousImage = 3,
-		PreviousSkipImage = 4,
-		RenameFile = 5,
-		RandomImage = 6,
-		ZoomIn = 7,
-		ZoomOut = 8,
-		ZoomDefault = 9,
-		ZoomFull = 10,
-		ZoomFree = 11,
-		PanUp = 12,
-		PanDown = 13,
-		PanLeft = 14,
-		PanRight = 15,
-		FirstImage = 16,
-		LastImage = 17,
-		RecycleCurrentFile = 18,  // Send file to recycle bin OR delete, with out without confirmation dialog, depending on OS settings.
-		DeleteCurrentFile = 19,   // Immediately deletes the file, bypassing any recycle bin.
-		RemoveCurrentImage = 19,  // Removes image from rotation, does NOT touch the actual file.
-		ToggleFullscreen = 20,
-		CloseApplication = 21,
-		OpenSettings = 22,
-		CopyImage = 23,
+		Undefined,
+		NextImage,
+		NextSkipImage,
+		PreviousImage,
+		PreviousSkipImage,
+		RenameFile,
+		RandomImage,
+		ZoomIn,
+		ZoomOut,
+		ZoomDefault,
+		ZoomFull,
+		ZoomFree,
+		PanUp,
+		PanDown,
+		PanLeft,
+		PanRight,
+		FirstImage,
+		LastImage,
+		RecycleCurrentFile,  // Send file to recycle bin OR delete, with out without confirmation dialog, depending on OS settings.
+		DeleteCurrentFile,   // Immediately deletes the file, bypassing any recycle bin.
+		RemoveCurrentImage,  // Removes image from rotation, does NOT touch the actual file.
+		ToggleFullscreen,
+		CloseApplication,
+		OpenSettings,
+		CopyImage,
 	};
 
 	struct KeyboardPress {
