a39c207fd1555025300fb745b8c02c977460ed9c
Pontus Mårdnäs <pontus@mardnas.se>
2015-10-12 21:42:27 +0200

    Remove VS-specific annotations

--- source/deps/orz/stream_mem.h
+++ source/deps/orz/stream_mem.h
@@ -7,17 +7,17 @@
 namespace IO {
 	class StreamMemory:public Stream {
 	public:
-		StreamMemory(_In_reads_bytes_(size) uint8_t* buffer, _In_ size_t size);
+		StreamMemory(uint8_t* buffer, size_t size);
 
 	private:
-		_Check_return_ std::string performName() const;
-		_Check_return_ bool performOpen();
-		_Check_return_ bool performIsOpen() const;
+		std::string performName() const;
+		bool performOpen();
+		bool performIsOpen() const;
 		void performClose();
-		_Check_return_ size_t performRead(_Out_writes_bytes_(size * items) void* buf, _In_ size_t size, _In_ size_t items);
-		void performSeek(_In_ FileInt position, _In_ SeekMethod m);
-		_Check_return_ FileInt performPosition() const;
-		_Check_return_ FileInt performSize();
+		size_t performRead(void* buf, size_t size, size_t items);
+		void performSeek(FileInt position, SeekMethod m);
+		FileInt performPosition() const;
+		FileInt performSize();
 
 		mutable std::recursive_mutex m_mutexAccess;
 
