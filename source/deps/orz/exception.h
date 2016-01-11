497f3bee5ef489490db9b9a1262af802fb80d6eb
Pontus Mårdnäs <pontus@mardnas.se>
2016-01-11 20:16:26 +0100

    Improve exception info on GCC/Clang

--- source/deps/orz/exception.h
+++ source/deps/orz/exception.h
@@ -37,10 +37,7 @@ namespace Err {
 
 std::string DoThrowBuildDescription(const char* filename, int line, const char* functionName, const std::string& description);
 
-#ifdef _MSC_VER
-#define DO_THROW(exception, description) throw exception(DoThrowBuildDescription(__FILE__, __LINE__, __FUNCTION__, description))
-#else
-#define DO_THROW(exception, description) throw exception(DoThrowBuildDescription("UNK_FILE", 0, "UNK_FUNC", std::string(description)))
-#endif
+// Works on MSVC, Clang, GCC
+#define DO_THROW(exception, description) throw exception(DoThrowBuildDescription(__FILE__, __LINE__, __FUNCTION__, std::string(description)))
 
 #endif
