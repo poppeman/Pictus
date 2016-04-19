9bb394ed6321bc0dd82cfcf5bc2bb5721cf8596c
Pontus Mårdnäs <pontus@mardnas.se>
2016-04-19 22:53:55 +0200

    Launch single-click handler if double-click is not assigned

--- source/Pictus/viewer.cpp
+++ source/Pictus/viewer.cpp
@@ -49,7 +49,13 @@ namespace App {
 		m_adjust.OnChange.connect([this](int a, int b, int c) { AdjustChange(a, b, c); });
 		OnMouseButtonDown.connect([this](Win::MouseEvent e) { return m_mouseMap.Execute(MouseStandardEvent(e, m_cfg.Mouse), e); });
 		OnMouseWheel.connect([this](Win::MouseEvent e) { return m_mouseMap.Execute(MouseStandardEvent(e, m_cfg.Mouse), e); });
-		OnMouseButtonDoubleClick.connect([this](Win::MouseEvent e) { return m_mouseMap.Execute(MouseDblEvent(e, m_cfg.Mouse), e); });
+		OnMouseButtonDoubleClick.connect([this](Win::MouseEvent e) {
+			auto ev = MouseDblEvent(e, m_cfg.Mouse);
+			if (ev == MouseAction::MouseUndefined || ev == MouseAction::MouseDisable) {
+				ev = MouseStandardEvent(e, m_cfg.Mouse);
+			}
+			return m_mouseMap.Execute(ev, e);
+		});
 		OnTaskbarButton.connect([this](int id) { PerformOnTaskbarButton(id); });
 
 		m_mouseMap.AddAction(MouseFullscreen, [this](Win::MouseEvent) { ToggleFullscreenMode(); });
