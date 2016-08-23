4a8394f11cb8a35d3b8ed6fa726824314b50269f
Pontus Mårdnäs <pontus@mardnas.se>
2016-08-23 21:42:45 +0200

    Prevent hang when codecs return a frame-delay of 0

--- source/Pictus/timer.cpp
+++ source/Pictus/timer.cpp
@@ -7,6 +7,7 @@ namespace Win {
 	}
 
 	void Timer::Create(int delay) {
+		delay = std::max(1, delay);  // Things pretty much hang if delay=0
 		if (!Start(delay))
 		{
 			DO_THROW(Err::CriticalError, "SetTimer Failed");
