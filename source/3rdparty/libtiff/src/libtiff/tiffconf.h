dc8c926b0356d0cc7fee329562418ddb7865d7e2
Pontus Mårdnäs <pontus@mardnas.se>
2016-09-10 19:17:18 +0200

    Make tiff work on 64-bit windows

--- source/3rdparty/libtiff/src/libtiff/tiffconf.h
+++ source/3rdparty/libtiff/src/libtiff/tiffconf.h
@@ -7,47 +7,91 @@
 #ifndef _TIFFCONF_
 #define _TIFFCONF_
 
-/* Signed 16-bit type */
-#define TIFF_INT16_T signed short
+#ifdef _WIN64
+	/* Signed 16-bit type */
+	#define TIFF_INT16_T signed short
 
-/* Signed 32-bit type */
-#define TIFF_INT32_T signed int
+	/* Signed 32-bit type */
+	#define TIFF_INT32_T signed int
 
-/* Signed 64-bit type */
-#define TIFF_INT64_T signed long long
+	/* Signed 64-bit type */
+	#define TIFF_INT64_T signed long long
 
-/* Signed 8-bit type */
-#define TIFF_INT8_T signed char
+	/* Signed 8-bit type */
+	#define TIFF_INT8_T signed char
 
-/* Unsigned 16-bit type */
-#define TIFF_UINT16_T unsigned short
+	/* Unsigned 16-bit type */
+	#define TIFF_UINT16_T unsigned short
 
-/* Unsigned 32-bit type */
-#define TIFF_UINT32_T unsigned int
+	/* Unsigned 32-bit type */
+	#define TIFF_UINT32_T unsigned int
 
-/* Unsigned 64-bit type */
-#define TIFF_UINT64_T unsigned long long
+	/* Unsigned 64-bit type */
+	#define TIFF_UINT64_T unsigned long long
 
-/* Unsigned 8-bit type */
-#define TIFF_UINT8_T unsigned char
+	/* Unsigned 8-bit type */
+	#define TIFF_UINT8_T unsigned char
 
-/* Unsigned size type */
-#define TIFF_SIZE_T unsigned int
+	/* Unsigned size type */
+	#define TIFF_SIZE_T unsigned long
 
-/* Signed size type */
-#define TIFF_SSIZE_T signed int
+	/* Signed size type */
+	#define TIFF_SSIZE_T signed long long
 
-/* Pointer difference type */
-#define TIFF_PTRDIFF_T ptrdiff_t
+	/* Pointer difference type */
+	#define TIFF_PTRDIFF_T ptrdiff_t
 
-/* Define to 1 if the system has the type `int16'. */
-/* #undef HAVE_INT16 */
+	/* Define to 1 if the system has the type `int16'. */
+	/* #undef HAVE_INT16 */
 
-/* Define to 1 if the system has the type `int32'. */
-/* #undef HAVE_INT32 */
+	/* Define to 1 if the system has the type `int32'. */
+	/* #undef HAVE_INT32 */
 
-/* Define to 1 if the system has the type `int8'. */
-/* #undef HAVE_INT8 */
+	/* Define to 1 if the system has the type `int8'. */
+	/* #undef HAVE_INT8 */
+#else
+	/* Signed 16-bit type */
+	#define TIFF_INT16_T signed short
+
+	/* Signed 32-bit type */
+	#define TIFF_INT32_T signed int
+
+	/* Signed 64-bit type */
+	#define TIFF_INT64_T signed long long
+
+	/* Signed 8-bit type */
+	#define TIFF_INT8_T signed char
+
+	/* Unsigned 16-bit type */
+	#define TIFF_UINT16_T unsigned short
+
+	/* Unsigned 32-bit type */
+	#define TIFF_UINT32_T unsigned int
+
+	/* Unsigned 64-bit type */
+	#define TIFF_UINT64_T unsigned long long
+
+	/* Unsigned 8-bit type */
+	#define TIFF_UINT8_T unsigned char
+
+	/* Unsigned size type */
+	#define TIFF_SIZE_T unsigned int
+
+	/* Signed size type */
+	#define TIFF_SSIZE_T signed int
+
+	/* Pointer difference type */
+	#define TIFF_PTRDIFF_T ptrdiff_t
+
+	/* Define to 1 if the system has the type `int16'. */
+	/* #undef HAVE_INT16 */
+
+	/* Define to 1 if the system has the type `int32'. */
+	/* #undef HAVE_INT32 */
+
+	/* Define to 1 if the system has the type `int8'. */
+	/* #undef HAVE_INT8 */
+#endif
 
 /* Compatibility stuff. */
 
