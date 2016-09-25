56b91294e56db783aae3f5ad1700573cc5e711f1
Pontus Mårdnäs <pontus@mardnas.se>
2016-09-25 20:57:55 +0200

    Make enter and cancel work in rename dialog

--- source/Pictus/rename.cpp
+++ source/Pictus/rename.cpp
@@ -14,9 +14,9 @@ namespace App
 	void Rename::OnOk()
 	{
 		auto newName = ToAString(m_filename->GetValue());
-
+		auto changed = (m_name != newName);
 		m_name = newName;
-		EndModal((newName != m_name));
+		EndModal(changed);
 	}
 
 	void Rename::OnCancel()
@@ -32,13 +32,13 @@ namespace App
 
 		auto upperSizer = new wxBoxSizer(wxHORIZONTAL);
 		upperSizer->Add(new wxStaticText(this, wxID_ANY, Win::GetStringWx(SIDRenameNewFilename)), wxSizerFlags(0));
-		m_filename = new wxTextCtrl(this, wxID_ANY, L"", wxDefaultPosition, { 400, wxDefaultCoord }, 0L, wxDefaultValidator);
+		m_filename = new wxTextCtrl(this, wxID_ANY, L"", wxDefaultPosition, { 400, wxDefaultCoord }, wxTE_PROCESS_ENTER, wxDefaultValidator);
 		upperSizer->Add(m_filename, wxSizerFlags(1).Border(wxLEFT, 20));
 
 		auto lowerSizer = new wxBoxSizer(wxHORIZONTAL);
-		auto okButton = new wxButton(this, wxID_ANY, Win::GetStringWx(SIDDialogOK));
+		auto okButton = new wxButton(this, wxID_OK, Win::GetStringWx(SIDDialogOK));
 		okButton->Bind(wxEVT_BUTTON, [&](wxCommandEvent& evt) { OnOk();  });
-		auto cancelButton = new wxButton(this, wxID_ANY, Win::GetStringWx(SIDDialogCancel));
+		auto cancelButton = new wxButton(this, wxID_CANCEL, Win::GetStringWx(SIDDialogCancel));
 		cancelButton->Bind(wxEVT_BUTTON, [&](wxCommandEvent& evt) { OnCancel(); });
 
 		lowerSizer->Add(okButton, wxSizerFlags(0));
@@ -51,5 +51,6 @@ namespace App
 		SetSizerAndFit(topSizer);
 
 		m_filename->SetValue(wxString::FromUTF8(m_name.c_str()));
+		m_filename->Bind(wxEVT_TEXT_ENTER, [=](wxCommandEvent& evt) { OnOk(); });
 	}
 }
